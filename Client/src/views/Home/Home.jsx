fa5e71ba2b7fce18eca543814a43ab6379448c89
Ignacio Bruna <joseignaciobruna95@gmail.com>
2024-01-04 23:56:58 -0300

    Solucion a busqueda duplicada

--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -5,24 +5,26 @@ import NavBar from "../../components/NavBar/NavBar";
 import { useState } from "react";
 import axios from "axios";
 
-
-
 const Home = () =>{
     const [characters, setCharacters] = useState([]);
+    const [addedCharacters, setAddedCharacters] = useState([]);
 
     function onClose(id){
         setCharacters(characters.filter((element) => element.id !== id));
+        setAddedCharacters((oldAddedChars) => oldAddedChars.filter(charId => charId !== id));
       }
 
       async function onSearch(character){
         try {
-          const { data } = await axios (`http://localhost:3001/rickandmorty/character/${character}`)
-            if(data.name){
+          const { data } = await axios (`http://localhost:3001/rickandmorty/character/${character}`);
+            if(data.name && !addedCharacters.includes(data.id)){
               setCharacters((oldChars) => [...oldChars, data]);
+              setAddedCharacters((oldAddedChars) => [...oldAddedChars, data.id]);
+            }else if(addedCharacters.includes(data.id)){
+              alert("Ese personaje ya fue agregado");
             }
           } catch (error) {
              alert('No hay personajes con ese ID');
-            
           }
         }
     return(
